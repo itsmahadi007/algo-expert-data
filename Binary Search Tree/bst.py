e48c33c925372d5c23b08d226a921ebce188deb5
Mahadi Hassan <itsmahadi@gmail.com>
2023-03-07 00:25:48 +0600

    y

--- Binary Search Tree/bst.py
+++ Binary Search Tree/bst.py
@@ -1,6 +1,6 @@
 class BinaryTreeNode:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data=None):
+        self.data = data if data else None
         self.leftChild = None
         self.rightChild = None
 
@@ -34,6 +34,48 @@ def search(root, value):
         return search(root.rightChild, value)
 
 
+def inorder(self):
+    if self is not None:
+        inorder(self.leftChild)
+        print(self.data, end=" ")
+        inorder(self.rightChild)
+
+
+def minValueNode(node):
+    current = node
+    while current.leftChild is not None:
+        current = current.leftChild
+    return current
+
+
+def delete(root, value):
+    if root is None:
+        return root
+    if value < root.data:
+        root.leftChild = delete(root.leftChild, value)
+    elif value > root.data:
+        root.rightChild = delete(root.rightChild, value)
+    else:
+        # if node to be deleted has no children
+        if root.leftChild is None and root.rightChild is None:
+            root = None
+        # if node to be deleted has only one child
+        elif root.leftChild is None:
+            temp = root
+            root = root.rightChild
+            temp = None
+        elif root.rightChild is None:
+            temp = root
+            root = root.leftChild
+            temp = None
+        # if node to be deleted has two children
+        else:
+            temp = minValueNode(root.rightChild)
+            root.data = temp.data
+            root.rightChild = delete(root.rightChild, temp.data)
+    return root
+
+
 root = insert(None, 50)
 insert(root, 20)
 insert(root, 53)
@@ -44,3 +86,4 @@ insert(root, 78)
 
 print("53 is present in the binary tree:", search(root, 53))
 print("100 is present in the binary tree:", search(root, 100))
+inorder(root)
--- test.py
+++ test.py
@@ -1,8 +1,28 @@
-import math
+from math import sqrt
 
-x = 100
-y = math.log(x)
-z = math.log10(x)
 
-print("The natural logarithm of x is:", y)
-print("The base-10 logarithm of x is:", z)
+def count_full_square_pairs(numbers):
+    n = len(numbers)
+    # Create a set to store the square values
+    squares = set(i * i for i in range(int(sqrt(2 * 10**5)) + 1))
+    # Initialize a counter for the number of full square pairs
+    count = 0
+    # Use a dictionary to store the frequency of each number in the array
+    frequency = {}
+    for num in numbers:
+        frequency[num] = frequency.get(num, 0) + 1
+    # Iterate through the frequency dictionary
+    for num, freq in frequency.items():
+        # Check if the sum of the current number and other numbers is a full square
+        for square in squares:
+            if square - num in frequency and square - num != num:
+                count += freq * frequency[square - num]
+        # Check if the sum of the current number and itself is a full square
+        if num >= 0 and 2 * num in squares:
+            count += freq * (freq - 1) // 2
+    return count
+
+
+numbers = [-1, 18, 3, 1, 5]
+result = count_full_square_pairs(numbers)
+print(result)
